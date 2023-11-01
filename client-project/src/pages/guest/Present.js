1de3eb9bd530ce20e5d9321190ab461e6d915a64
Manuel Patiño <manuel121938@gmail.com>
2023-10-31 23:59:59 -0500

    Cambios en spa

---
+++ client-project/src/pages/guest/Present.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Present.scss';
+
+const Present = () => {
+    return (
+        <div className='hero'>
+            <div className='content'>
+                <p>Pagina, proyecto de desarrollo de software</p>
+                <p>Pagina realizada para proyecto de desarrollo de software</p>
+            </div>
+            <img
+                src='https://seeklogo.com/images/D/dragon-ball-goku-logo-7A0B308D2D-seeklogo.com.png'
+                alt='image'
+                className='image'
+            ></img>
+        </div>
+    );
+};
+
+export default Present;
