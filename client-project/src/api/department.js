4e0cf2090df0af24b821796a4e92820d28af2ef5
Esteban Patiño <esteban.pg45@gmail.com>
2023-10-31 21:33:29 -0500

    Uso de cache para guardar departamentos y municipios

--- client-project/src/api/department.js
+++ client-project/src/api/department.js
@@ -4,61 +4,63 @@ const { BASE_API_URL, API_ROUTER } = ENV;
 export class Department{
     getDepartments = async () => {
         try {
-            const response = await fetch("https://www.datos.gov.co/resource/xdk5-pm3f.json");
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            const data = await response.json();
-    
-            const uniqueData = [];
-            const departamentoSet = {};
-            for (const item of data) {
-                const departamento = item.departamento;
-                if (!departamentoSet[departamento]) {
-                    departamentoSet[departamento] = true;
-                    uniqueData.push({ label: departamento, value: departamento });
+
+            if(localStorage.getItem("depAndMuns") === null ){
+                const response = await fetch("https://www.datos.gov.co/resource/xdk5-pm3f.json");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
                 }
+                let data = await response.json();
+                const uniqueData = [];
+                const departamentoSet = {};
+                for (const item of data) {
+                    const departamento = item.departamento;
+                    if (!departamentoSet[departamento]) {
+                        departamentoSet[departamento] = true;
+                        uniqueData.push({ label: departamento, value: departamento });
+                    }
+                }
+                let depAndMun = []
+                uniqueData.sort((a, b) => a.label.localeCompare(b.label));
+                
+                for (let i = 0; i < uniqueData.length; i++) {
+                    
+                    const response = await fetch(`https://www.datos.gov.co/resource/xdk5-pm3f.json?departamento=${uniqueData[i].value}`);
+                    const data = await response.json();
+
+                    const municipioSet = {};
+                    const uniqueMunicipios = [];
+                    for (const item of data) {
+                        if (item.departamento === uniqueData[i].value) {
+                            const municipio = item.municipio;
+                            if (!municipioSet[municipio]) {
+                                municipioSet[municipio] = true;
+                                uniqueMunicipios.push({ label: municipio, value: municipio });
+                            }
+                        }
+                    }
+                    uniqueMunicipios.sort((a, b) => a.label.localeCompare(b.label));
+        
+                    const auxDep = {
+                        department: uniqueData[i],
+                        municipios: uniqueMunicipios
+                    }
+                    depAndMun.push(auxDep)
+                }
+                
+                localStorage.setItem("depAndMuns", JSON.stringify(depAndMun));
+                return localStorage.getItem("depAndMuns")
+            }else {
+                return localStorage.getItem("depAndMuns")
             }
-            uniqueData.sort((a, b) => a.label.localeCompare(b.label));
-            return uniqueData;
+            
         } catch (err) {
             console.error(err);
             return []; // Return an empty array in case of an error
         }
     };    
   
-    getMunByDepartment = async (department) => {
-        try {
-            const response = await fetch(`https://www.datos.gov.co/resource/xdk5-pm3f.json?departamento=${department}`);
     
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            const data = await response.json();
-    
-            const uniqueMunicipios = [];
-            const municipioSet = {};
-    
-            for (const item of data) {
-                if (item.departamento === department) {
-                    const municipio = item.municipio;
-                    if (!municipioSet[municipio]) {
-                        municipioSet[municipio] = true;
-                        uniqueMunicipios.push({ label: municipio, value: municipio });
-                    }
-                }
-            }
-    
-            // Ordenar uniqueMunicipios alfabéticamente por la propiedad 'label'
-            uniqueMunicipios.sort((a, b) => a.label.localeCompare(b.label));
-    
-            return uniqueMunicipios;
-    
-        } catch (error) {
-            console.error(error);
-            return [{ label: 'Error en el servidor', value: 'Error en el servidor' }];
-        }
-    }
     
     createAddress = async (data) => {
         const response = await fetch(`${BASE_API_URL}${API_ROUTER.CREATE_ADDRESS}`, {
